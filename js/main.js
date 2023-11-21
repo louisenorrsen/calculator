da77166aca332b700a49028e69076511716c8e59
Louise Norrsén <louisenorrsen@outlook.com>
2023-11-21 10:40:05 +0100

    Fix calculator functionality

--- js/main.js
+++ js/main.js
@@ -2,29 +2,27 @@ const buttons = document.querySelectorAll('.buttons');
 const history = document.querySelector('.display-history');
 const result = document.querySelector('.display-result');
 
+let mostRecentlyPushedButton = '';
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         if (button.textContent === '+/-') {
-            tooglePositiveOrNegative();
-        } else if (button.textContent === '/' || button.textContent === 'x' || button.textContent === '-' || button.textContent === '+') {
+            togglePositiveOrNegative();
+        } else if (button.textContent === '/' || button.textContent === '*' || button.textContent === '-' || button.textContent === '+') {
             addOperator(button);
         } else if (button.textContent === '=') {
             calculate();
         } else if (button.textContent === 'C') {
             erase();
         } else {
-            if (result.textContent === '0' && button.textContent !== '.') {
-                result.textContent = button.textContent;
-            } else {
-                result.textContent += button.textContent;
-            }
+            checkMostRecentlyPushedButton(button);
         }
+        mostRecentlyPushedButton = button.textContent;
     });
 });
 
-const tooglePositiveOrNegative = () => {
-    if (result.textContent !== '' || result.textContent !== '0') {
-        result.textContent = result.textContent * -1;
+const togglePositiveOrNegative = () => {
+    if (result.textContent !== '' && result.textContent !== '0') {
+        result.textContent = String(result.textContent * -1);
     }
 }
 
@@ -41,6 +39,18 @@ const calculate = () => {
 
 const addOperator = (button) => {
     history.textContent += `${result.textContent} ${button.textContent}`;
-    result.textContent = '';
+}
+
+const checkMostRecentlyPushedButton = (button) => {
+    if (result.textContent === '0' && button.textContent !== '.') {
+        result.textContent = button.textContent;
+    } else if (mostRecentlyPushedButton === 'C' || mostRecentlyPushedButton === '+' || mostRecentlyPushedButton === '-' || mostRecentlyPushedButton === '*' || mostRecentlyPushedButton === '/') {
+        result.textContent = button.textContent;
+    } else if (mostRecentlyPushedButton === '=') {
+        result.textContent = button.textContent;
+        history.textContent = '';
+    } else {
+        result.textContent += button.textContent;
+    }
 }
 
